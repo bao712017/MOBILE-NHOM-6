d3400fe765a7deda56e83a66b9623c65860ec509
Minh Nguyen <22520871@gm.uit.edu.vn>
2025-11-17 22:30:55 +0700

    Add Google Sign In C#

---
+++ Twisted tower/Assets/Scripts/GoogleAuthManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GoogleAuthManager : MonoBehaviour
+{
+    public void OnClickGoogleSignIn()
+    {
+        Debug.Log("Google Sign-In button clicked!");
+
+        // TODO: Sau này bạn sẽ add code kết nối Google/Firebase ở đây.
+        // Ví dụ: StartCoroutine(GoogleSignInFlow());
+
+        // Tạm thời cứ log ra để test là nút đã hoạt động.
+    }
+}
