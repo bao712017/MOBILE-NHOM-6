5d0ada8a3435eee6a0ebd4cf8825f7734273463d
Minh Nguyen <22520871@gm.uit.edu.vn>
2025-11-21 23:55:56 +0700

    clear conflict

--- Twisted tower/Assets/Scripts/GoogleAuthManager.cs
+++ Twisted tower/Assets/Scripts/GoogleAuthManager.cs
@@ -1,14 +1,93 @@
 ﻿using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Google;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
 
 public class GoogleAuthManager : MonoBehaviour
 {
+    public static GoogleAuthManager Instance;
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+
+    private string webClientId =
+        "783975870997-2ltboggapl6o2v2o4q2pk3j0irdd2cj8.apps.googleusercontent.com";
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+
+    private async void Start()
+    {
+        await InitializeFirebase();
+    }
+
+    private async System.Threading.Tasks.Task InitializeFirebase()
+    {
+        var dependency = await FirebaseApp.CheckAndFixDependenciesAsync();
+        if (dependency == DependencyStatus.Available)
+        {
+            auth = FirebaseAuth.DefaultInstance;
+            Debug.Log("Firebase Ready!");
+        }
+        else
+            Debug.LogError("Firebase error: " + dependency);
+    }
+
     public void OnClickGoogleSignIn()
     {
-        Debug.Log("Google Sign-In button clicked!");
+#if UNITY_ANDROID
+        SignInWithGoogle();
+#endif
+    }
 
-        // TODO: Sau này bạn sẽ add code kết nối Google/Firebase ở đây.
-        // Ví dụ: StartCoroutine(GoogleSignInFlow());
+    private void SignInWithGoogle()
+    {
+        GoogleSignInConfiguration config = new GoogleSignInConfiguration
+        {
+            WebClientId = webClientId,
+            RequestIdToken = true
+        };
 
-        // Tạm thời cứ log ra để test là nút đã hoạt động.
+        GoogleSignIn.Configuration = config;
+
+        GoogleSignIn.DefaultInstance.SignIn()
+            .ContinueWith(OnGoogleAuthFinished);
+    }
+
+    private void OnGoogleAuthFinished(System.Threading.Tasks.Task<GoogleSignInUser> task)
+    {
+        if (task.IsFaulted)
+        {
+            Debug.LogError("Google Sign-In failed: " + task.Exception);
+            return;
+        }
+
+        var googleUser = task.Result;
+        string idToken = googleUser.IdToken;
+
+        Credential credential = GoogleAuthProvider.GetCredential(idToken, null);
+
+        auth.SignInWithCredentialAsync(credential)
+            .ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                    Debug.LogError("Firebase auth failed: " + t.Exception);
+                else
+                {
+                    user = t.Result;
+                    Debug.Log("LOGIN OK: " + user.DisplayName);
+
+                    SceneManager.LoadScene("Menu");
+                }
+            });
     }
 }
